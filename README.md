16565ac611710a4c765e0add3b9f41f3d0d82b28
raxshasa <ollypercival@gmail.com>
2021-08-17 12:41:59 +0100

    Adds netlify status

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # world-cup-of-names
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d654e76a-ceb7-4d5e-ae04-332cbb4bf976/deploy-status)](https://app.netlify.com/sites/sleepy-swanson-5e60e1/deploys)
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,6 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 import Link from 'next/link'
 
-export default function Index() {
+export default function About() {
   return <h1>About</h1>;
 }
